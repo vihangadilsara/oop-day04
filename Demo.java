6b0f572e6d0984c3097167a85c4f61c213dc5371
vihangadilsara <vdilsara30@gmail.com.com>
2026-07-05 09:55:19 +0530

    Refactor Stack class: update constructor, improve push and pop methods, and enhance display functionality

--- Demo.java
+++ Demo.java
@@ -1,59 +1,55 @@
 class Stack{
 	private int[] dataArray;
-	
-	Stack(){
-		dataArray=new int[0];
+	private int nextIndex;
+	Stack(int size){
+		dataArray=new int[size];
+		nextIndex=0;
 	}
 	private void extendsArray(){
-		int[] tempDataArray=new int[dataArray.length+1];
-		for (int i = 0; i < dataArray.length; i++){
-			tempDataArray[i+1]=dataArray[i];
-		}
-		dataArray=tempDataArray;
+	
+	}
+	private boolean isFull(){
+		return nextIndex>=dataArray.length;
 	}
 	public void push(int data){
-		extendsArray();
-		dataArray[0]=data;	
+		if(isFull()){
+			extendsArray();
+		}
+		dataArray[nextIndex++]=data;
 	}
 	public void pop(){
 		if(!isEmpty()){
-			int[] tempDataArray=new int[dataArray.length-1];
-			for (int i = 0; i < tempDataArray.length; i++){
-				tempDataArray[i]=dataArray[i+1];
-			}
-			dataArray=tempDataArray;	
+			nextIndex--;
 		}
 	}
 	public int search(int data){
-		for(int i=0; i<dataArray.length; i++){
-			if(dataArray[i]==data){
-				return i;
-			}
-		}
+		//Insert code here
 		return -1;
 	}
 	public void display(){
 		System.out.print("[");
-		for(int i=0; i<dataArray.length;i++){
-			System.out.print(dataArray[i]+", ");		
+		for (int i =nextIndex-1; i>=0 ; i--){
+			System.out.print(dataArray[i]+", ");
 		}
-		System.out.println(isEmpty()? "empty":"\b\b]");
+		System.out.println(isEmpty()? "empty]":"\b\b]");
 	}
 	public boolean isEmpty(){
-		return dataArray.length<=0;
+		return nextIndex<=0;
 	}
 }
 class Demo{
 	public static void main(String args[]){
-		Stack s1=new Stack();
+		Stack s1=new Stack(100);
 		s1.push(10);
 		s1.push(20);
 		s1.push(30);
 		s1.push(40);
 		s1.push(50);
 		s1.display(); //[50,40,30,20,10]
+		s1.pop();
+		s1.display(); //[40,30,20,10]
 		
-		System.out.println("Index of 40 : "+s1.search(40));	//1
+		System.out.println("Index of 40 : "+s1.search(40));	//-1
 
 	}
 }
