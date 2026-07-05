6e3c31007fcb4690a4ee547f9cb14e3d7ced9a21
vihangadilsara <vdilsara30@gmail.com.com>
2026-07-05 10:54:46 +0530

    Refactor Queue class: update peek and poll methods, enhance remove functionality, and improve display method

--- Demo.java
+++ Demo.java
@@ -21,10 +21,15 @@ class Queue{
 		return nextIndex>=dataArray.length;
 	}
 	public int peek(){
-		return isEmpty() ? -1: dataArray[nextIndex-1]; 
+		return isEmpty() ? -1: dataArray[0]; 
 	}
 	public int poll(){
-		return isEmpty() ? -1:  dataArray[--nextIndex]; 
+		if(!isEmpty()){
+			int firstData=dataArray[0];
+			remove();
+			return firstData;
+		}
+		return -1;
 	}
 	public void add(int data){
 		if(isFull()){
@@ -52,15 +57,25 @@ class Queue{
 	}
 	public void remove(){
 		if(!isEmpty()){
-			
+			for (int i = 0; i <nextIndex-1; i++){
+				dataArray[i]=dataArray[i+1];
+			}
+			nextIndex--;
 		}
 	}
 	public int search(int data){
-		//Insert code here
+		for (int i = 0; i < nextIndex; i++){
+			if(dataArray[i]==data){
+				return i;
+			}
+		}
 		return -1;
 	}
 	public void display(){
 		System.out.print("[");
+		for (int i = 0; i < nextIndex; i++){
+			System.out.print(dataArray[i]+", ");
+		}
 		
 		System.out.println(isEmpty()? "empty]":"\b\b]");
 	}
@@ -92,8 +107,16 @@ class Demo{
 		q1.remove();
 		q1.display(); //[20,30,40,50 ........120]
 		System.out.println("Size      : "+q1.size()); //11
-		
 		System.out.println("Index of 50 : "+q1.search(50)); //3
 		
+		int firstData=q1.peek();
+		System.out.println("First data : "+firstData);
+		System.out.println("Calling after peek()....");
+		q1.display(); //[20,30,40,50 ........120]
+		
+		firstData=q1.poll();
+		System.out.println("First data : "+firstData);
+		System.out.println("Calling after poll()....");
+		q1.display(); //[30,40,50 ........120]
 	}
 }
