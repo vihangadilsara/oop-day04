9a9d0f5f7e1d12d7cb0a6ae0e397a9d09b384707
vihangadilsara <vdilsara30@gmail.com.com>
2026-07-05 13:17:51 +0530

    Refactor Student and StudentList classes: implement student management functionality, including add, remove, and search methods

--- Demo.java
+++ Demo.java
@@ -1,122 +1,156 @@
-class Queue{
-	private int[] dataArray;
+class Student{
+	private String id;	
+	private String name;	
+	private int prfMarks;
+	private int dbmsMarks;
+	
+	Student(){
+		
+	}
+	Student(String id, String name, int prfMarks, int dbmsMarks){
+		this.id=id;
+		this.name=name;
+		this.prfMarks=prfMarks;
+		this.dbmsMarks=dbmsMarks;
+	}
+	public String getId(){
+		return id;
+	}
+	public boolean equals(Student student){
+		return id.equalsIgnoreCase(student.id);
+	}
+	public String toString(){
+		return "["+id+","+name+","+prfMarks+","+dbmsMarks+"]";
+	}
+}
+class StudentList{ //Collection of student objects
+	private Student[] studentArray;
 	private int nextIndex;
 	private double loadFact;
 	private int initSize;
 	
-	Queue(int initSize, double loadFact){
-		dataArray=new int[initSize];
+	StudentList(int initSize, double loadFact){
+		studentArray=new Student[initSize];
 		nextIndex=0;
 		this.loadFact=loadFact;
 		this.initSize=initSize;
 	}
 	private void extendsArray(){
-		int tempDataArray[]=new int[dataArray.length+(int)(loadFact*dataArray.length)];
-		for (int i = 0; i < dataArray.length; i++){
-			tempDataArray[i]=dataArray[i];
+		Student tempStudentArray[]=new Student[studentArray.length+(int)(loadFact*studentArray.length)];
+		for (int i = 0; i < studentArray.length; i++){
+			tempStudentArray[i]=studentArray[i];
 		}
-		dataArray=tempDataArray; 
+		studentArray=tempStudentArray; 
 	}
 	private boolean isFull(){
-		return nextIndex>=dataArray.length;
+		return nextIndex>=studentArray.length;
 	}
-	public int peek(){
-		return isEmpty() ? -1: dataArray[0]; 
-	}
-	public int poll(){
-		if(!isEmpty()){
-			int firstData=dataArray[0];
-			remove();
-			return firstData;
-		}
-		return -1;
-	}
-	public void add(int data){
+
+
+
+	public void add(Student student){ //Insertion order
 		if(isFull()){
 			extendsArray();
 		}
-		dataArray[nextIndex++]=data;
+		studentArray[nextIndex++]=student;
+	}
+	public void add(int index,Student student){
+		if(isFull()){
+			extendsArray();
+		}
+		if(index>=0 && index<=nextIndex){
+			for (int i = nextIndex-1; i >=index; i--){
+				studentArray[i+1]=studentArray[i];
+			}
+			studentArray[index]=student;
+			nextIndex++;
+		}
+	}
+	public void addFirst(Student student){
+		add(0,student);
+	}
+	public void addLast(Student student){
+		add(nextIndex,student);
 	}
 	public void trimToSize(){
-		int[] tempDataArray=new int[size()];
-		for (int i = 0; i < dataArray.length; i++){
-			tempDataArray[i]=dataArray[i];
+		Student[] tempStudentArray=new Student[size()];
+		for (int i = 0; i < studentArray.length; i++){
+			tempStudentArray[i]=studentArray[i];
 		}
-		dataArray=tempDataArray;
+		studentArray=tempStudentArray;
 	}
 	public int size(){
 		return nextIndex;
 	}
-	public void trim(){
-		//
-		//
-	}
+
 	public void clear(){
-		dataArray=new int[initSize];
+		studentArray=new Student[initSize];
 		nextIndex=0;
 	}
-	public void remove(){
+	public void remove(int index){
 		if(!isEmpty()){
-			for (int i = 0; i <nextIndex-1; i++){
-				dataArray[i]=dataArray[i+1];
-			}
+			if(index>=0 && index<nextIndex){
+				for(int i=index; i<nextIndex-1; i++){
+					studentArray[i]=studentArray[i+1];
+				}
+			}	
 			nextIndex--;
 		}
 	}
-	public int search(int data){
+	public void removeFirst(){
+		remove(0);
+	}
+
+	public void removeLast(){
+		remove(size()-1);
+	}
+	public int search(Student student){
 		for (int i = 0; i < nextIndex; i++){
-			if(dataArray[i]==data){
+			if(student.equals(studentArray[i])){
 				return i;
 			}
 		}
 		return -1;
 	}
 	public void display(){
-		System.out.print("[");
+		System.out.print("{");
 		for (int i = 0; i < nextIndex; i++){
-			System.out.print(dataArray[i]+", ");
+			System.out.print(studentArray[i].toString()+", ");
 		}
+		System.out.println(isEmpty()? "empty]":"\b\b}");
+	}
+	public void display(int start){
 		
-		System.out.println(isEmpty()? "empty]":"\b\b]");
+	}
+	public void display(int startIndex, int endIndex){
+		
+	}
+	public Student get(int index){
+		return null;
+	}
+	public Student getFirst(){
+		return null;
+	}
+	public Student getLast(){
+		return null;
 	}
 	public boolean isEmpty(){
 		return nextIndex<=0;
 	}
 	public int capacity(){
-		return dataArray.length;
+		return studentArray.length;
 	}
 }
 class Demo{
 	public static void main(String args[]){
-		Queue q1=new Queue(12,0.25);
-		q1.add(10);
-		q1.add(20);
-		q1.add(30);
-		q1.add(40);
-		q1.add(50);
-		q1.add(60);
-		q1.add(70);
-		q1.add(80);
-		q1.add(90);
-		q1.add(100);
-		q1.add(110);
-		q1.add(120);
-		q1.display(); //[10,20,30,40,50 ........120]
-		System.out.println("Size      : "+q1.size()); //12
+		StudentList stList=new StudentList(12,0.25);
+		stList.add(new Student("S0001","Nimal",65,67));
+		stList.add(new Student("S0002","Amal",85,70));
+		stList.add(new Student("S0003","Bimal",35,30));
+		stList.add(new Student("S0004","Ramal",55,70));
+		stList.add(new Student("S0005","Anil",95,90));
+		stList.display(); //{[S0001,Nimal,65,76], [S0002","Amal",85,70]...}
 		
-		q1.remove();
-		q1.display(); //[20,30,40,50 ........120]
-		System.out.println("Size      : "+q1.size()); //11
-		System.out.println("Index of 50 : "+q1.search(50)); //3
-		
-		int firstData=q1.peek();
-		System.out.println("First data : "+firstData);
-		System.out.println("Calling after peek()....");
-		q1.display(); //[20,30,40,50 ........120]
-		
-		firstData=q1.poll();
-		System.out.println("First data : "+firstData);
-		System.out.println("Calling after poll()....");
-		q1.display(); //[30,40,50 ........120]
+		System.out.println("Index of S003-Bimal : "+stList.search(new Student("S0003","Bimal",35,30))); //2
 	}
 }
