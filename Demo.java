aa88b1cc685e27679d864218c9c23cd72150ac16
vihangadilsara <vdilsara30@gmail.com.com>
2026-07-05 10:35:17 +0530

    Refactor Stack class: enhance constructor, implement dynamic array resizing, and add peek, poll, trimToSize, and capacity methods

--- Demo.java
+++ Demo.java
@@ -1,21 +1,54 @@
 class Stack{
 	private int[] dataArray;
 	private int nextIndex;
-	Stack(int size){
-		dataArray=new int[size];
+	private double loadFact;
+	private int initSize;
+	
+	Stack(int initSize, double loadFact){
+		dataArray=new int[initSize];
 		nextIndex=0;
+		this.loadFact=loadFact;
+		this.initSize=initSize;
 	}
 	private void extendsArray(){
-	
+		int tempDataArray[]=new int[dataArray.length+(int)(loadFact*dataArray.length)];
+		for (int i = 0; i < dataArray.length; i++){
+			tempDataArray[i]=dataArray[i];
+		}
+		dataArray=tempDataArray; 
 	}
 	private boolean isFull(){
 		return nextIndex>=dataArray.length;
+	}
+	public int peek(){
+		return isEmpty() ? -1: dataArray[nextIndex-1]; 
+	}
+	public int poll(){
+		return isEmpty() ? -1:  dataArray[--nextIndex]; 
 	}
 	public void push(int data){
 		if(isFull()){
 			extendsArray();
 		}
 		dataArray[nextIndex++]=data;
+	}
+	public void trimToSize(){
+		int[] tempDataArray=new int[size()];
+		for (int i = 0; i < dataArray.length; i++){
+			tempDataArray[i]=dataArray[i];
+		}
+		dataArray=tempDataArray;
+	}
+	public int size(){
+		return nextIndex;
+	}
+	public void trim(){
+		//
+		//
+	}
+	public void clear(){
+		dataArray=new int[initSize];
+		nextIndex=0;
 	}
 	public void pop(){
 		if(!isEmpty()){
@@ -36,20 +69,38 @@ class Stack{
 	public boolean isEmpty(){
 		return nextIndex<=0;
 	}
+	public int capacity(){
+		return dataArray.length;
+	}
 }
 class Demo{
 	public static void main(String args[]){
-		Stack s1=new Stack(100);
+		Stack s1=new Stack(12,0.25);
 		s1.push(10);
 		s1.push(20);
 		s1.push(30);
 		s1.push(40);
 		s1.push(50);
-		s1.display(); //[50,40,30,20,10]
-		s1.pop();
-		s1.display(); //[40,30,20,10]
+		s1.push(60);
+		s1.push(70);
+		s1.push(80);
+		s1.push(90);
+		s1.push(100);
+		s1.push(110);
+		s1.push(120);
+		s1.display(); //[120,110,100,90...50,40,30,20,10]
+		System.out.println("Size      : "+s1.size()); //12
 		
-		System.out.println("Index of 40 : "+s1.search(40));	//-1
-
+		int topData;
+		topData=s1.peek();
+		System.out.println("Top Data : "+topData); //120
+		System.out.println("After calling peek()...");
+		s1.display(); //[120,110,100,90...50,40,30,20,10]
+		
+		topData=s1.poll();
+		System.out.println("Top Data : "+topData); //120
+		System.out.println("After calling poll()...");
+		s1.display(); //[110,100,90...50,40,30,20,10]
+		
 	}
 }
