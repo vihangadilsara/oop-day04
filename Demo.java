24f3358b1c9719702c918c5423654418f1d2d488
vihangadilsara <vdilsara30@gmail.com.com>
2026-07-05 09:36:10 +0530

    Fix Stack class methods: correct array handling in push and pop, and implement search functionality

--- Demo.java
+++ Demo.java
@@ -7,23 +7,34 @@ class Stack{
 	private void extendsArray(){
 		int[] tempDataArray=new int[dataArray.length+1];
 		for (int i = 0; i < dataArray.length; i++){
-			tempDataArray[i]=dataArray[i];
+			tempDataArray[i+1]=dataArray[i];
 		}
 		dataArray=tempDataArray;
 	}
 	public void push(int data){
 		extendsArray();
-		dataArray[dataArray.length-1]=data;	
+		dataArray[0]=data;	
 	}
 	public void pop(){
-		
+		if(!isEmpty()){
+			int[] tempDataArray=new int[dataArray.length-1];
+			for (int i = 0; i < tempDataArray.length; i++){
+				tempDataArray[i]=dataArray[i+1];
+			}
+			dataArray=tempDataArray;	
+		}
 	}
 	public int search(int data){
+		for(int i=0; i<dataArray.length; i++){
+			if(dataArray[i]==data){
+				return i;
+			}
+		}
 		return -1;
 	}
 	public void display(){
 		System.out.print("[");
-		for(int i=dataArray.length-1; i>=0; i--){
+		for(int i=0; i<dataArray.length;i++){
 			System.out.print(dataArray[i]+", ");		
 		}
 		System.out.println(isEmpty()? "empty":"\b\b]");
@@ -42,7 +53,7 @@ class Demo{
 		s1.push(50);
 		s1.display(); //[50,40,30,20,10]
 		
-		s1.pop();
-		s1.display(); //[40,30,20,10]
+		System.out.println("Index of 40 : "+s1.search(40));	//1
+
 	}
 }
