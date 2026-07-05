f72b259fd59ee4ea1e4eb94777979cb5a3243ab1
vihangadilsara <vdilsara30@gmail.com.com>
2026-07-05 09:01:21 +0530

    Implement Stack class with push, pop, search, and display methods

---
+++ Demo.java
@@ -0,0 +1,40 @@
+class Stack{
+	private int[] dataArray;
+	
+	public void push (int data){
+		
+		}
+	public void pop(){
+		
+		}
+	
+	public int search(int data){
+		return -1;
+		}
+	
+	public void display(){
+		
+		}
+	}
+	
+	class Demo{
+		public static void main(String args[]){
+			Stack s1=new Stack();
+			s1.push(10);
+			s1.push(20);
+			s1.push(30);
+			s1.push(40);
+			s1.push(50);
+			s1.display();
+			
+			s1.pop();
+			s1.display();
+			
+			
+			
+			
+			}
+		
+		
+		
+		}
